9889c93b0eab391e1b90d6fe8f227ed2eac55f80
adilraja11 <adilraja19@students.undip.ac.id>
2024-03-17 16:03:24 +0700

    minor change on route.js activity

--- src/app/api/v1/activity/route.js
+++ src/app/api/v1/activity/route.js
@@ -1,17 +1,17 @@
 import prisma from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request, { params }) {
-  // handling request
-  const userId = params.id;
+// export async function GET(request, { params }) {
+//   // handling request
+//   const userId = params.id;
 
-  // business logic
-  const userData = await prisma.activity.findFirst({
-      where: {
-          id: userId
-      }
-  });
+//   // business logic
+//   const userData = await prisma.activity.findFirst({
+//       where: {
+//           id: userId
+//       }
+//   });
 
-  // handling response
-  return NextResponse.json({message: `GET Actiivty List Detail Success`, data: userData});
-}
+//   // handling response
+//   return NextResponse.json({message: `GET Actiivty List Detail Success`, data: userData});
+// }
