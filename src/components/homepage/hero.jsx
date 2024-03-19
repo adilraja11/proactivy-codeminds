be221c6adf42d84ee0312ec46b8427d63cea7c42
nadriha <hafez.nadhira@gmail.com>
2024-03-19 12:00:55 +0700

    features: add hero in homepage

--- src/components/homepage/hero.jsx
+++ src/components/homepage/hero.jsx
@@ -4,11 +4,29 @@ export const Hero = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
     <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+        <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="w-full max-w-sm rounded-lg shadow-2xl" />
         <div>
-        <h1 className="text-5xl font-bold">Box Office News!</h1>
-        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button className="btn btn-primary">Get Started</button>
+            <h1 className="text-5xl font-bold">Every Success starts with a little knowledge</h1>
+            <p className="py-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pulvinar massa vel odio lobortis consequat.</p>
+            
+            
+            <div className="stats stats-vertical lg:stats-horizontal shadow">
+            <div className="stat">
+                <div className="stat-value">99+</div>
+                <div className="stat-desc">User</div>
+            </div>
+            
+            <div className="stat">
+                <div className="stat-value">1K+</div>
+                <div className="stat-desc">Activity</div>
+            </div>
+            
+            <div className="stat">
+                <div className="stat-value">6</div>
+                <div className="stat-desc">Category</div>
+            </div>
+            
+            </div>
         </div>
     </div>
     </div>
