1146e4b1767bf1e3a93b046a51c75f7d80e038da
nadriha <hafez.nadhira@gmail.com>
2024-03-23 16:20:17 +0700

    features: search and filters activities

--- src/components/activity/feature.jsx
+++ src/components/activity/feature.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { SearchBar } from './searchbar';
 import { Dropdown } from './dropdown';
+import { CategoryDropdown } from './CategoryDropdown';
 
-export const MainComponent = () => {
+export const MainComponent = ({categoryData, onChangeCategoryId, onChangeSearch, categoryId}) => {
     return (
         <div>
-            <div className="flex justify-between items-center">
-                <SearchBar />
-                <Dropdown />
+            <div className="flex justify-between items-center gap-4">
+                <SearchBar onChange={onChangeSearch}/>
+                <CategoryDropdown data={categoryData} onChange={onChangeCategoryId} categoryId={categoryId}/>
             </div>
         </div>
     );
