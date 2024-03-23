fd5c93bc6ea79c7a4e487d34482305e339fe87c5
nadriha <hafez.nadhira@gmail.com>
2024-03-24 00:01:54 +0700

    fix: homepage ui

--- src/components/activity/activitycard.jsx
+++ src/components/activity/activitycard.jsx
@@ -6,17 +6,22 @@ import React from 'react';
 
 export const ActivityCard = ({ id, title, slug, category, categoryId, description, featuredImage, images, pricing }) => {
     return (
-        <div className="card bg-white shadow-xl text-black">
-            <figure>
-                <Image src="https://fakeimg.pl/440x320/" width={440} height={320} alt="Shoes" className="object-cover hover:scale-105 transition duration-300" />
-            </figure>
+        <div className="card bg-white shadow-xl text-black flex flex-col justify-between">
             <div className="card-body">
-                <p className="text-xs font-semibold text-primary">{category.name}</p>
-                <h2 className="card-title">
-                    {title}
-                    <div className="badge badge-primary badge-sm">{pricing}</div>
-                </h2>
-                <p>{description}</p>
+                <div>
+                    <p className="text-xs font-semibold text-primary">{category.name}</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <h2 className="card-title">{title}</h2>
+                    {pricing == "FREE"? <div className="badge badge-success badge-sm">{pricing}</div> : <div className="badge badge-error badge-sm">{pricing}</div>}
+                </div>
+
+                <div>
+                    <p className="line-clamp-3">{description}</p>
+                </div>
+                
+            </div>
+            <div className="m-4">
                 <Link href={`/${categoryId}/${slug}`}>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">See Details</button>
--- src/components/homepage/hero.jsx
+++ src/components/homepage/hero.jsx
@@ -1,34 +1,35 @@
-import React from 'react'
+    import React from 'react'
+    import Image from 'next/image'
 
-export const Hero = () => {
-  return (
-    <div className="hero min-h-screen">
-    <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="w-full max-w-sm rounded-lg shadow-2xl" />
-        <div>
-            <h1 className="text-6xl font-bold">Every Success starts with a little knowledge</h1>
-            <p className="py-6 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pulvinar massa vel odio lobortis consequat.</p>
-            
-            
-            <div className="w-full max-w-xl stats stats-vertical lg:stats-horizontal shadow">
-            <div className="stat text-center">
-                <div className="stat-value">99+</div>
-                <div className="stat-desc">User</div>
-            </div>
-            
-            <div className="stat text-center">
-                <div className="stat-value">1K+</div>
-                <div className="stat-desc">Activity</div>
-            </div>
-            
-            <div className="stat text-center">
-                <div className="stat-value">6</div>
-                <div className="stat-desc">Category</div>
-            </div>
-            
+    export const Hero = () => {
+    return (
+        <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+            <Image src="/hero.svg" alt="Hero" width={500} height={300}/>
+            <div>
+                <h1 className="text-6xl font-bold">Every Success starts with a little knowledge</h1>
+                <p className="py-6 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pulvinar massa vel odio lobortis consequat.</p>
+                
+                
+                <div className="w-full max-w-xl stats stats-vertical lg:stats-horizontal shadow">
+                <div className="stat text-center">
+                    <div className="stat-value">99+</div>
+                    <div className="stat-desc">User</div>
+                </div>
+                
+                <div className="stat text-center">
+                    <div className="stat-value">1K+</div>
+                    <div className="stat-desc">Activity</div>
+                </div>
+                
+                <div className="stat text-center">
+                    <div className="stat-value">6</div>
+                    <div className="stat-desc">Category</div>
+                </div>
+                
+                </div>
             </div>
         </div>
-    </div>
-    </div>
-  )
-}
+        </div>
+    )
+    }
